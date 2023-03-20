fb216052057fef75640f25ae4d763d90ade2b601
mill2808 <105799175+mill2808@users.noreply.github.com>
2023-03-20 11:26:47 -0400

    Test deploy commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Helen 
+          Learn React with Helen and MSU!
         </a>
       </header>
     </div>
